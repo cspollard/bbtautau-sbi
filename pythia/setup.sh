45628f2bf62ccab3a79338367bbb0a8ccc5a0f6f
Chris Pollard <cpollard@cern.ch>
2023-08-19 16:13:04 +0100

    yeah

--- pythia/setup.sh
+++
@@ -1,5 +0,0 @@
-export RIVETPATH=../../rivet/local/
-# export PYTHIAPATH=../../pythia/pythia8309/
-export RIVET_ANALYSIS_PATH=$PWD
-
-. $RIVETPATH/rivetenv.sh
---
+++ rivet/setup.sh
@@ -0,0 +1,5 @@
+export RIVETPATH=../../rivet/local
+export MGPATH=../../MG5_aMC_v3_5_1
+export RIVET_ANALYSIS_PATH=$PWD
+
+. $RIVETPATH/rivetenv.sh
