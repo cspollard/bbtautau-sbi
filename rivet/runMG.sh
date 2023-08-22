34fb6e81502b3194c9e1aa7de279ec666e8c70dd
Chris Pollard <cspollard@gmail.com>
2023-08-22 14:45:10 +0100

    about to start a run

--- rivet/runMG.sh
+++ rivet/runMG.sh
@@ -1,3 +1,3 @@
-for f in higgs ZH DY HH
+for f in ttbar higgs ZH DY HH
   do $MGPATH/bin/mg5_aMC -f $f.MG
 done
--- rivet/setup.sh
+++ rivet/setup.sh
@@ -2,5 +2,4 @@ export MGPATH=$HOME/Physics/MG5_aMC_v3_5_1
 export RIVETPATH=$HOME/Physics/rivet/local
 export RIVET_ANALYSIS_PATH=$PWD
 
-. $RIVETPATH/rivetenv.sh
-sh /cvmfs/sft.cern.ch/lcg/releases/LCG_104/ROOT/6.28.04/x86_64-centos7-gcc12-opt/ROOT-env.sh
+sh $RIVETPATH/rivetenv.sh
