a5af4dc564507231dbd33290cd4f5e3d067d0325
Chris Pollard <cspollard@gmail.com>
2023-08-24 21:28:19 +0100

    inference is working?

--- inference/train.py
+++ inference/train.py
@@ -9,7 +9,6 @@ import optax
 
 import awkward
 from numpy import genfromtxt
-import numpy as onp
 
 from einops import repeat, rearrange, reduce
 
@@ -19,7 +18,7 @@ NBATCHES = 128
 BATCHSIZE = 64
 NMAX = 512
 LR = 1e-3
-NTEST = 256
+NTEST = 1024
 
 
 def splitkey(k):
@@ -41,14 +40,14 @@ def MLP(features, norms, activations):
   return Sequential([x for pair in laypairs for x in pair])
 
 
-perjet = MLP([16]*4 , [LayerNorm()]*3 + [id] , [relu]*3 + [softmax])
-perevt = MLP([32]*4 , [LayerNorm()]*3 + [id] , [relu]*3 + [softmax])
-inference = MLP([32]*4 + [2] , [LayerNorm()]*4 + [id] , [relu]*4 + [id])
+perjet = MLP([32]*4 , [id]*4 , [relu]*3 + [softmax])
+perevt = MLP([32]*4 , [id]*4 , [relu]*3 + [softmax])
+inference = MLP([32]*4 + [2] , [id]*5 , [relu]*4 + [id])
 
 
 params = \
   { "perjet" : perjet.init(PRNGKey(0), numpy.ones((1, 5)))
-  , "perevt" : perevt.init(PRNGKey(0), numpy.ones((1, 16)))
+  , "perevt" : perevt.init(PRNGKey(0), numpy.ones((1, 32)))
   , "inference" : inference.init(PRNGKey(1), numpy.ones((1, 32)))
   }
 
@@ -76,7 +75,6 @@ def forward(params, inputs, evtmasks, jetmasks):
   expandedevtmasks = \
     repeat(evtmasks, "b e -> b e f", f=unflattened.shape[2])
 
-  # here we also need an event mask...
   summed = reduce(unflattened * expandedevtmasks, "b e f -> b f", "sum")
 
   return inference.apply(params["inference"], summed)
@@ -103,9 +101,11 @@ def readarr(fname):
 
   mask = numpy.any(arr == 999, axis=2)
 
-  arr[:,:,2:5] = arr[:,:,2:5] / 20e3
+  # divide momenta by 20 GeV
+  arr[:,:,2:5] = arr[:,:,2:5] / 20
 
-  return arr, mask
+
+  return arr , mask
 
 
 datasets = { k : readarr(k + ".csv") for k in [ "top" , "HH" ] }
@@ -145,8 +145,8 @@ def sample(k, lams, maxn):
   return tmp , mask
 
 
-ttlams = 200 / len(datasets["top"][0]) * numpy.ones((len(datasets["top"][0]),))
-hhlams = 10 / len(datasets["HH"][0]) * numpy.ones((len(datasets["HH"][0]),))
+ttlams = 100 / len(datasets["top"][0]) * numpy.ones((len(datasets["top"][0]),))
+hhlams = 1 / len(datasets["HH"][0]) * numpy.ones((len(datasets["HH"][0]),))
 evts = numpy.concatenate([ datasets["top"][0] , datasets["HH"][0] ])
 masks = numpy.concatenate([ datasets["top"][1] , datasets["HH"][1] ])
 
@@ -163,10 +163,10 @@ def appmu(k, mus, maxn):
 
 
 def prior(k, b):
-  return 10 * random.uniform(k, shape=(b,))
+  return 100 * random.uniform(k, shape=(b,))
 
 def testprior(k, b):
-  return 2.5 + 5 * random.uniform(k, shape=(b,))
+  return 25 + 50 * random.uniform(k, shape=(b,))
 
 
 
@@ -197,21 +197,16 @@ def step(params, opt_state, batch, evtmasks, jetmasks, labels):
   return params, opt_state, loss_value
 
 
-sched = optax.cosine_onecycle_schedule(NEPOCHS*NBATCHES, 1)
+sched = optax.cosine_decay_schedule(LR , NEPOCHS*NBATCHES)
 
-optimizer = \
-  optax.chain \
-  ( optax.scale(-LR)
-  , optax.scale_by_adam()
-  , optax.scale_by_schedule(sched)
-  )
+optimizer = optax.adam(learning_rate=sched)
 
 opt_state = optimizer.init(params)
 
 knext = PRNGKey(10)
 
 k , knext = splitkey(knext)
-testlabels = testprior(k, NTEST)
+testlabels = prior(k, NTEST)
 k , knext = splitkey(knext)
 testidxs , testevtmasks = appmu(k, testlabels, NMAX)
 testbatch , testjetmasks = evts[testidxs] , masks[testidxs]
@@ -230,7 +225,16 @@ for _ in range(NEPOCHS):
 
   outs = numpy.exp(forward(params, testbatch, testevtmasks, testjetmasks))
 
-  pull = (outs[:,0] - testlabels) / outs[:,1]
+  diff = outs[:,0] - testlabels
+  pull = diff / outs[:,1]
+  print("labels and sample outputs")
+  print(testlabels[:5])
+  print(outs[:5,0])
+  print(outs[:5,1])
+  print()
+  print("sample diffs")
+  print(diff[:5])
+  print()
   print("sample pulls")
   print(pull[:5])
   print()
