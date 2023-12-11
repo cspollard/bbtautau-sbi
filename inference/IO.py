5c4d21a61e49e7faf33c9487c32081baa1f6f65d
Chris Pollard <cspollard@gmail.com>
2023-12-11 09:31:28 +0000

    start propagating through

--- inference/IO.py
+++ inference/IO.py
@@ -12,29 +12,30 @@ def readarr(xsectimeslumi, fname, maxjets):
   
   events = awkward.run_lengths(arr[:,0])
 
-  # TODO I'm not sure about this, but I think it should get the jet information.
-  arr = awkward.unflatten(arr[1:7], events)
+  jets = awkward.unflatten(arr[:,1:7], events)
 
   # TODO here we read in the weights
-  wgts = awkward.unflatten(arr[7:], events)
+  wgts = awkward.unflatten(arr[:,7:], events)
 
-  arr = \
+  jets = \
     awkward.fill_none \
-    ( awkward.pad_none( arr , maxjets , clip=True , axis=1 )
+    ( awkward.pad_none( jets , maxjets , clip=True , axis=1 )
     , [999]*6
     , axis=1
     )
 
-  arr = awkward.to_regular(arr).to_numpy().astype(numpy.float32)[:,:,1:]
+  # TODO is this correct?
+  jets = awkward.to_regular(jets).to_numpy().astype(numpy.float32)
 
-  mask = numpy.any(arr != 999, axis=2)
+  mask = numpy.any(jets != 999, axis=2)
 
   # divide momenta by 20 GeV (just a good normalization guess)
-  arr[:,:,2:5] = arr[:,:,2:5] / 20
+  jets[:,:,2:5] = jets[:,:,2:5] / 20
 
   l = len(mask)
-  weights = xsectimeslumi / l * numpy.ones((l,))
+  weights = wgts / l
 
+  # problem seems to be here.
   return \
     mixturedict \
     ( { "events" : array(arr), "jetmasks" : array(mask) }
